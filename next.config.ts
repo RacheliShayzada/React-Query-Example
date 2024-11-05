37144be69be95fb95e5dba6cc1a1ca604241da39
RacheliShayzada <054racheli@gmail.com>
2024-11-05 11:15:05 +0200

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+//  @type {import('next').NextConfig}
+import type { NextConfig } from 'next';
+
+const url = "mongodb+srv://054racheli:AB502sO6yjU9TWoZ@cluster0.08t3l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  env: {
+    PUBLIC_DB_CONNECTION: url,
+  },
+}
 
 export default nextConfig;
